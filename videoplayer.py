4bdaa4a7ba867b83e7c4b7e29b6c063435c9ef1e
sanatwalia896 <121603801+sanatwalia896@users.noreply.github.com>
2023-04-22 15:50:19 +0530

    Add files via upload

---
+++ videoplayer.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import datetime
+import tkinter as tk
+from tkinter import filedialog
+from tkVideoPlayer import TkinterVideo
+
+
+root=tk.Tk()
+root.title("𝕭𝖆𝖉𝖍𝖎𝖗 𝕾𝖚𝖓")
+root.geometry("800*700+290+10")
+
+f
+
+
+
+
+root.mainloop()
