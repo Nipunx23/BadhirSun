6e9e89b7b9fddf93aba7212c1973d8508a6425f7
Nipunx23 <65994596+Nipunx23@users.noreply.github.com>
2023-04-22 17:59:32 +0530

    Update txt_to_isl.py

--- txt_to_isl.py
+++ txt_to_isl.py
@@ -1,2 +1,16 @@
+import spacy
 
-sub_txt = input("Subtitle text: ")
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("he went to school and they went to club.")
+new = ""
+for token in doc:
+    if token.pos_ == "CCONJ":
+        new += '. '
+    else:
+        new += " "+token.text; 
+doc = nlp(new[1:])
+for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+            token.shape_, token.is_alpha, token.is_stop)
+
+print(doc)
