8fe1d088e8cbc3704332ea0311031d0a62f5471b
anshika-kamboj <111910883+anshika-kamboj@users.noreply.github.com>
2023-04-22 21:55:59 +0530

    EDIT2 tkinter videoplayer + IMAGES

--- videoplayer.py
+++ videoplayer.py
@@ -12,7 +12,7 @@ root.geometry("800x700+290+10")
 frame =tk.Frame(root)
 frame.pack()
 
-image_icon = PhotoImage(file= "BadhirSunLogo.gif")
+image_icon = PhotoImage(file= "images\BadhirSunLogo.gif")
 root.iconphoto(False,image_icon)
 
 lower_frame = tk.Frame(root,bg="#FFFFFF")
@@ -23,4 +23,15 @@ loat_btn.pack(ipadx=12 , ipady=4, anchor = tk.NE)
 
 vid_player = TkinterVideo(root, scaled = True)
 vid_player.pack(expand = True, fill = "both")
+
+Button_bckwrd = PhotoImage(file="images\backward button.png")
+backb = tk.Button(lower_frame,image=Button_bckwrd , bd=0 ,height=50 , width = 50 , command = lambda:skip(-5)).pack(side = LEFT)
+
+Button_pp = PhotoImage(file="images\pp_button.jpg")
+play_pause_btn = tk.Button(lower_frame , image= Button_pp, bd=0 ,height=50, width=50 , command = play_pause)
+play_pause_btn.pack(expand = True,fill = "both" , side = LEFT)
+
+Button_frwrd = PhotoImage(file="images\forward button.png")
+backf = tk.Button(lower_frame,image=Button_frwrd , bd=0 ,height=50 , width = 50 , command = lambda:skip(5)).pack(side = RIGHT)
+
 root.mainloop()
