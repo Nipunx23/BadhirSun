5f0f84cda9723603b9d81313434472bd5c22249f
sanatwalia896 <121603801+sanatwalia896@users.noreply.github.com>
2023-04-22 10:55:21 +0530

    Updating Window

--- Window.py
+++ Window.py
@@ -5,5 +5,14 @@ class Video_player(tk.Frame):
     def __init__(self,parent,*args,**kwargs):
         tk.Frame.__init__(self,arent,*args,**kwargs)
 
+        self.instance=vlc.Instance("--no-xlib")
+
+        self.player=self.instance.media_player_new()
+
+        self.video=th.Canvas(self,bg="black")
+
+        self.video.pack(side="top",fill="both",expand=True)
+
+    def open_file(self,fileth):
         
 
