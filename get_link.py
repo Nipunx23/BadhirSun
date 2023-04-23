8c67c9b652bba1572275c9273c22ad80d939359c
Nipunx23 <65994596+Nipunx23@users.noreply.github.com>
2023-04-23 23:06:43 +0530

    Update get_link.py

--- get_link.py
+++ get_link.py
@@ -2,16 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 word = "random"
-url = "https://indiansignlanguage.org/"+word
+url = "https://www.youtube.com/@isldictionary/search?query="+word
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
-yt_link = ''
-for Links in soup.find_all("meta",content = True):
-    print(Links['content'])
-    if 'youtube' in Links:
-        yt_link = Links
-        break
-
-print(yt_link)
+all_links = soup.find_all('a')
+for links in all_links:
+    print(links['href'])
